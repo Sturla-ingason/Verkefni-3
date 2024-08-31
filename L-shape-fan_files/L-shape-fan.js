7eb16f2c9a0afa7ae8bc45036ee2fa351b676080
Sturla-ingason <Sturladi@gmail.com>
2024-08-31 19:25:16 +0000

    Þríhyrningur sem býr til L

--- L-shape-fan_files/L-shape-fan.js
+++ L-shape-fan_files/L-shape-fan.js
@@ -14,12 +14,15 @@ window.onload = function init()
     gl = WebGLUtils.setupWebGL( canvas );
     if ( !gl ) { alert( "WebGL isn't available" ); }
 
-    var vertices = new Float32Array([-0.75, -0.75,
-                                     -0.75,  0.75,
-                                     -0.35,  0.75,
-                                     -0.35, -0.35,
-                                      0.45, -0.35,
-                                      0.45, -0.75]);
+    var vertices = new Float32Array([-0.75, 0.75,
+                                        -0.35,  0.75,
+                                        -0.75, -0.75,
+                                        -0.35, -0.75,
+                                        -0.35, -0.45,
+                                        0.45, -0.45,
+                                        0.45, -0.75,
+                                        -0.35, -0.75
+                                        ]);
 
     //  Configure WebGL
 
@@ -49,5 +52,5 @@ window.onload = function init()
 
 function render() {
     gl.clear( gl.COLOR_BUFFER_BIT );
-    gl.drawArrays( gl.TRIANGLE_FAN, 0, 6 );
+    gl.drawArrays( gl.TRIANGLE_STRIP, 0, 8 );
 }
